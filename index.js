d101553689be1a0b4a864ea928d69ced3f3e2332
Justice Mubayiwa <j.mubayiwa@gmail.com>
2021-09-01 11:31:55 -0600

    if error is thrown getting background image show default image

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         document.body.style.backgroundImage = `url(${data.urls.full})`
         document.getElementById("author").textContent = `By: ${data.user.name}`
     })
+    .catch(err => {
+
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1511195448930-75ffebe8e3a8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzA1MTc0NTM&ixlib=rb-1.2.1&q=80&w=1080)`
+
+    })
