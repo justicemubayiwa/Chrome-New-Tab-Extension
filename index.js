968da95001163a4cca538862d30fba37b1f8156e
Justice Mubayiwa <j.mubayiwa@gmail.com>
2021-09-01 12:48:03 -0600

    Feth ethereum logo and name to display top left

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
     document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1511195448930-75ffebe8e3a8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzA1MTc0NTM&ixlib=rb-1.2.1&q=80&w=1080)`
 
     })
+
+    fetch("https://api.coingecko.com/api/v3/coins/ethereum")
+    .then(res => {
+      if(!res.ok){
+        throw Error('Something went wrong')
+      }
+      return res.json()
+    })
+    .then(data => {
+      document.getElementById("crypto-top").innerHTML = `
+            <img src=${data.image.small} />
+            <span>${data.name}</span>
+            `
+          })
+          .catch(err => console.error(err))
