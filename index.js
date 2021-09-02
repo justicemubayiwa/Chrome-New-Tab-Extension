403a9f9a6ea2fc00811d859ab3a106ad67458480
Justice Mubayiwa <j.mubayiwa@gmail.com>
2021-09-02 11:23:08 -0600

    HTML spacing for ease of reading. added another div for bottom

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=rome")
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=gold")
     .then(res => res.json())
     .then(data => {
         document.body.style.backgroundImage = `url(${data.urls.full})`
